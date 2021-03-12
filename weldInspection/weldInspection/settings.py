fcbcba4bb42f079419c30f6cdef2c0e2d7b7f35f
AshRavi7 <ashravi7@gmail.com>
2021-03-12 17:20:57 +0530

    add ons

--- weldInspection/weldInspection/settings.py
+++ weldInspection/weldInspection/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projectReport.apps.AppConfig'
+    'projectReport.apps.ProjectreportConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

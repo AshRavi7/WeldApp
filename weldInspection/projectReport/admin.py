bd9c37f01f319de29d884863fdd111416c2da9ac
AshRavi7 <ashravi7@gmail.com>
2021-03-12 17:19:38 +0530

    registered models

--- weldInspection/projectReport/admin.py
+++ weldInspection/projectReport/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import project,inspec_user,location_discipline,weld_action,heat_calc,drawing,weld,gallery,activity_description,activity_inspection_action
 
 # Register your models here.
+admin.site.register(inspec_user)
+admin.site.register(project)
+admin.site.register(location_discipline)
+admin.site.register(weld_action)
+admin.site.register(heat_calc)
+admin.site.register(drawing)
+admin.site.register(weld)
+admin.site.register(gallery)
+admin.site.register(activity_description)
+admin.site.register(activity_inspection_action)
+
